7f83605b96214abf0fa79794276cdfe1d6bd046b
venkadesh.s <venkadesh.s@knackforge.com>
2022-09-13 16:28:24 +0530

    Added save and update feature

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,10 +1,28 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor(private _http:HttpClient) { }
+  constructor(private _http: HttpClient) { }
+
+  username = 'venkadeshseenu';
+  repo = 'github-file-Save';
+  token = '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';
+
+  reqHeader = new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ' + this.token
+  });
+
+  //  https://docs.github.com/en/rest/repos/contents#create-or-update-file-contents
+
+  getSha() {
+    return this._http.get('https://api.github.com/repos/venkadeshseenu/github-file-Save/contents/test1.yml', { headers: this.reqHeader });
+  }
+  save(body: any) {
+    return this._http.put('https://api.github.com/repos/venkadeshseenu/github-file-Save/contents/test1.yml', body, { headers: this.reqHeader })
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Document } from 'yaml';
+import { ApiService } from './api.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,15 +10,45 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent {
   title = 'github-file-upload';
   gitUrl = 'https://api.github.com/repos';
-  constructor(private http:HttpClient){
-
+  payload = {};
+  constructor(private api: ApiService) {
   }
-  // https://api.github.com/repos/batman/toys/contents/sample.pdf
-
   // /repos/{owner}/{repo}/contents/{path}
+  // https://github.com/venkadeshseenu/github-file-Save/tree/master
 
+  ngOnInit() {
+    // this.saveFile();
+  }
 
-  ngOnInit(){
+  saveFile() {
+    // https://www.npmjs.com/package/yaml
+    // https://eemeli.org/yaml/#creating-documents
+    const doc = new Document(['some', 'values', { balloons: 99 }])
+    doc.commentBefore = ' A commented document'
+    console.log(String(doc));
+    let text = btoa(String(doc))
+    console.log(text);
+    this.api.getSha().subscribe((res: any) => {
+      console.log(res);
 
+      let sha = res.sha;
+      if (sha) {
+        //update 
+        this.payload = { "message": "my commit message", "sha": sha, "committer": { "name": "venkadesh.s", "email": "venkadesh.s@knackforge.com" }, "content": text }
+        this.api.save(this.payload).subscribe((res) => {
+          console.log(res);
+          alert('Updated')
+        })
+      }
+    }, (err) => {
+      if (err.status == 404) {
+        //insert
+        this.payload = { "message": "my commit message", "committer": { "name": "venkadesh.s", "email": "venkadesh.s@knackforge.com" }, "content": text }
+        this.api.save(this.payload).subscribe((res) => {
+          console.log(res);
+          alert('Added')
+        })
+      }
+    })
   }
 }
